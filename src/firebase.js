e444d0a2c43d4d9943d729f7a1ed3f301af55f15
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-04-05 09:42:25 +0200

    initial commit main page created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXIZ-5TMirX_3h1Om1Fbo5W5j8GiHlxjA",
+  authDomain: "netflix-clone-35733.firebaseapp.com",
+  projectId: "netflix-clone-35733",
+  storageBucket: "netflix-clone-35733.appspot.com",
+  messagingSenderId: "94014953621",
+  appId: "1:94014953621:web:34f5381c975f9a461fe39c"
+};
